225d6b676b50cff368760e3c9c6be7c8f9212cb6
Mevlüt Buçan <59297282+mevlutbucan@users.noreply.github.com>
2024-02-04 11:14:17 +0300

    fix(client): modify auth reducer actions

--- client/src/app/services/redux/auth/reducer.ts
+++ client/src/app/services/redux/auth/reducer.ts
@@ -6,11 +6,13 @@ import { changeAuthState, signIn, signOut, signUp } from './actions';
 export const AuthReducer = createReducer(initialState, (builder) => {
   builder
     .addCase(changeAuthState, (state, action) => {
+      state.loggedIn = true;
       state.accessToken = action.payload.accessToken;
-      state.loggedIn = !!action.payload.accessToken;
+      localStorage.setItem('access_token', action.payload.accessToken);
     })
     .addCase(signOut, (state, action) => {
       state.loggedIn = false;
+      state.accessToken = undefined;
       localStorage.removeItem('access_token');
     })
     .addCase(signIn.pending, (state, action) => {
@@ -19,11 +21,11 @@ export const AuthReducer = createReducer(initialState, (builder) => {
       state.error = '';
     })
     .addCase(signIn.fulfilled, (state, action) => {
-      state.loggedIn = true;
       state.loading = false;
       state.success = true;
+      state.loggedIn = true;
       state.accessToken = action.payload.accessToken;
-      localStorage.setItem('access_token', state.accessToken);
+      localStorage.setItem('access_token', action.payload.accessToken);
     })
     .addCase(signIn.rejected, (state, action) => {
       state.loading = false;
@@ -35,11 +37,11 @@ export const AuthReducer = createReducer(initialState, (builder) => {
       state.error = '';
     })
     .addCase(signUp.fulfilled, (state, action) => {
-      state.loggedIn = true;
       state.loading = false;
       state.success = true;
+      state.loggedIn = true;
       state.accessToken = action.payload.accessToken;
-      localStorage.setItem('access_token', state.accessToken);
+      localStorage.setItem('access_token', action.payload.accessToken);
     })
     .addCase(signUp.rejected, (state, action) => {
       state.loading = false;
